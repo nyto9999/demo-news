7576afaedd519df1ea3d6b9a65fe143e1221f21c
ninjachen <nyto9999@gmail.com>
2022-07-20 03:23:03 +0800

    commit

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 宇宣 Chen on 2022/7/20.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
---
+++ demoViewModel/demoViewModel.h
@@ -0,0 +1,18 @@
+//
+//  demoViewModel.h
+//  demoViewModel
+//
+//  Created by 宇宣 Chen on 2022/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demoViewModel.
+FOUNDATION_EXPORT double demoViewModelVersionNumber;
+
+//! Project version string for demoViewModel.
+FOUNDATION_EXPORT const unsigned char demoViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoViewModel/PublicHeader.h>
+
+
